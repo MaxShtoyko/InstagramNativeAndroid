f50d9510ccaa5d913036fcee0998558cdd988243
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-15 10:40:05 +0300

    Added registration page

---
+++ Ins/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Core.ViewModels;
+
+namespace Ins.ViewModels
+{
+    public class RegistrationViewModel:MvxViewModel 
+    {
+        public ICommand OnSignUp { get; private set; }
+
+        public RegistrationViewModel()
+        {
+            OnSignUp = new MvxCommand(SignUpClicked);
+        }
+
+        void SignUpClicked()
+        {
+            ShowViewModel<TabbedViewModel>();
+        }
+    }
+}
---
+++ Ins/Views/RegistrationView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace Ins.Views
+{
+    [Activity]
+    public class RegistrationView : MvxActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.RegistrationPage);
+            // Create your application here
+        }
+    }
+}
