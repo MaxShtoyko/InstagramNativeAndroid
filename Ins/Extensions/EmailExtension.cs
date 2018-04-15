921069e59d248d82fb18bfaa867800da04ae9015
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-15 10:41:14 +0300

    Added User Model

---
+++ Ins/Extensions/EmailExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Ins.Extensions
+{
+    static public class EmailExtension
+    {
+        static public bool IsEmail( this string text )
+        {
+            try{
+                MailAddress mailAddress = new MailAddress(text);
+                return true;
+            }
+            catch (FormatException){
+                return false;
+            }
+        }
+    }
+}
---
+++ Ins/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Ins.Extensions;
+using Ins.Models;
+
+namespace Ins.Services
+{
+    static public class UserService
+    {
+        static public bool IsCorrect( User user )
+        {
+            if (String.IsNullOrWhiteSpace(user.Email) || String.IsNullOrWhiteSpace(user.Password)){
+                return false;
+            }
+
+            return user.Email.IsEmail() && user.Password.Length > 6;
+        }
+    }
+}
