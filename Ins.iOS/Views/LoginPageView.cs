76c08ca7839cc6023c2b38715dd9fc6f07d22992
Danylo Labenskyi <danylo.labenskyi@jetsoftpro.com>
2018-04-24 00:48:59 +0300

    Created initial iOS solution

---
+++ Ins.iOS/Views/LoginPageView.cs
@@ -0,0 +1,40 @@
+﻿using Ins.Core.ViewModels;
+using MvvmCross.iOS.Views;
+using MvvmCross.Binding.BindingContext;
+
+namespace Ins.iOS.Views
+{
+    public partial class LoginPageView : MvxViewController
+    {
+        protected LoginPageViewModel LoginPageViewModel
+            => ViewModel as LoginPageViewModel;
+
+        public LoginPageView() : base("LoginPageView", null)
+        {
+        }
+
+        protected void CreateBindings(){
+            var set = this.CreateBindingSet<LoginPageView, LoginPageViewModel>();
+
+            set.Bind(EmailTextField)
+               .To(vm => vm.User.Email).TwoWay();
+
+            set.Bind(PasswordTextField)
+               .To(vm => vm.User.Password).TwoWay();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            CreateBindings();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ Ins.iOS/Views/LoginPageView.designer.cs
@@ -0,0 +1,69 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Ins.iOS.Views
+{
+    [Register ("LoginPageView")]
+    partial class LoginPageView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField EmailTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton FacebookLoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LogInButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SignUpButton { get; set; }
+
+        [Action ("UIButton141_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton141_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (EmailTextField != null) {
+                EmailTextField.Dispose ();
+                EmailTextField = null;
+            }
+
+            if (FacebookLoginButton != null) {
+                FacebookLoginButton.Dispose ();
+                FacebookLoginButton = null;
+            }
+
+            if (LogInButton != null) {
+                LogInButton.Dispose ();
+                LogInButton = null;
+            }
+
+            if (PasswordTextField != null) {
+                PasswordTextField.Dispose ();
+                PasswordTextField = null;
+            }
+
+            if (SignUpButton != null) {
+                SignUpButton.Dispose ();
+                SignUpButton = null;
+            }
+        }
+    }
+}
