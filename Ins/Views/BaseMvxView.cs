bfadafd63ac16a82c352a72db6ea795296618556
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-25 16:58:30 +0300

    Added font files

---
+++ Ins/Views/BaseMvxView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Calligraphy;
+using MvvmCross.Droid.Views;
+
+namespace Ins.Droid.Views
+{
+    [Activity(Label = "BaseMvxView")]
+    public class BaseMvxView : MvxActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void AttachBaseContext(Android.Content.Context @base)
+        {
+            base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
+        }
+    }
+}
