7f1bfef1b76742b39822a5cff395e14a3746d728
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-25 14:38:13 +0300

    Implemented UI for LoginPage on Android

--- Ins/Views/LoginPageView.cs
+++ Ins/Views/LoginPageView.cs
@@ -1,27 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Android.App;
-using Android.Content;
+using Android.Graphics;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
-using Android.Views;
 using Android.Widget;
-using MvvmCross.Droid.Views;
 
 namespace Ins.Droid.Views
 {
     [Activity(Label = "LoginPageView",Theme = "@android:style/Theme.DeviceDefault.NoActionBar.Fullscreen", MainLauncher = true)]
-    public class LoginPageView : MvxActivity
+    public class LoginPageView : BaseMvxView
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.LoginPage);
 
+            var header = FindViewById<TextView>(Resource.Id.headerView);
+            var emailEntry = FindViewById<EditText>(Resource.Id.emailEntry);
+            var passwordEntry = FindViewById<EditText>(Resource.Id.passwordEntry);
+            var loginView = FindViewById<RelativeLayout>(Resource.Id.loginView);
+            var signUpButton = FindViewById<Button>(Resource.Id.signUpButton);
+            var logInButton = FindViewById<Button>(Resource.Id.logInButton);
+            var logInViaFacebookButton = FindViewById<Button>(Resource.Id.logInViaFacebookButton);
+
+            Typeface headerFont = Typeface.CreateFromAsset(Assets, "fonts/9424.ttf");
+            header.SetTypeface(headerFont, TypefaceStyle.Normal);
+
+            Typeface robotoLight = Typeface.CreateFromAsset(Assets, "fonts/Roboto-Light.ttf");
+            emailEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
+
+            emailEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
+            passwordEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
+            signUpButton.SetTypeface(robotoLight, TypefaceStyle.Normal);
+
+            passwordEntry.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            emailEntry.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            loginView.SetBackgroundResource(Resource.Drawable.LoginPageStyle);
+            logInButton.SetBackgroundResource(Resource.Drawable.ButtonStyle);
+            logInViaFacebookButton.SetBackgroundResource(Resource.Drawable.FacebookButtonStyle);
+            signUpButton.Background = null;
             // Create your application here
         }
     }
