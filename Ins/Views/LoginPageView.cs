03f3df4547d7323e46e053232d2b8e623d4ac382
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-25 16:40:26 +0300

    Implemented basic UI for Registration Page on Android

--- Ins/Views/LoginPageView.cs
+++ Ins/Views/LoginPageView.cs
@@ -14,6 +14,12 @@ namespace Ins.Droid.Views
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.LoginPage);
 
+            SetViews();
+            // Create your application here
+        }
+
+        private void SetViews()
+        {
             var header = FindViewById<TextView>(Resource.Id.headerView);
             var emailEntry = FindViewById<EditText>(Resource.Id.emailEntry);
             var passwordEntry = FindViewById<EditText>(Resource.Id.passwordEntry);
@@ -23,21 +29,18 @@ namespace Ins.Droid.Views
             var logInViaFacebookButton = FindViewById<Button>(Resource.Id.logInViaFacebookButton);
 
             Typeface headerFont = Typeface.CreateFromAsset(Assets, "fonts/9424.ttf");
+            Typeface robotoLightFont = Typeface.CreateFromAsset(Assets, "fonts/Roboto-Light.ttf");
+
             header.SetTypeface(headerFont, TypefaceStyle.Normal);
-
-            Typeface robotoLight = Typeface.CreateFromAsset(Assets, "fonts/Roboto-Light.ttf");
-            emailEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
-
-            emailEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
-            passwordEntry.SetTypeface(robotoLight, TypefaceStyle.Normal);
-            signUpButton.SetTypeface(robotoLight, TypefaceStyle.Normal);
+            emailEntry.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            passwordEntry.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            signUpButton.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
 
             passwordEntry.SetBackgroundResource(Resource.Drawable.EditTextStyle);
             emailEntry.SetBackgroundResource(Resource.Drawable.EditTextStyle);
             loginView.SetBackgroundResource(Resource.Drawable.LoginPageStyle);
             logInButton.SetBackgroundResource(Resource.Drawable.ButtonStyle);
             logInViaFacebookButton.SetBackgroundResource(Resource.Drawable.FacebookButtonStyle);
-            // Create your application here
         }
     }
 }
--- Ins/Views/RegistrationPageView.cs
+++ Ins/Views/RegistrationPageView.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
@@ -14,14 +15,42 @@ using MvvmCross.Droid.Views;
 
 namespace Ins.Droid.Views
 {
-    [Activity]
+    [Activity(Theme = "@android:style/Theme.DeviceDefault.NoActionBar.Fullscreen")]
     public class RegistrationPageView : MvxActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.RegistrationPage);
+
+            SetViews();
             // Create your application here
+        }
+
+        private void SetViews()
+        {
+            var NameEditText = FindViewById<EditText>(Resource.Id.registrationFullName);
+            var LoginEditText = FindViewById<EditText>(Resource.Id.registrationLogin);
+            var EmailEditText = FindViewById<EditText>(Resource.Id.registrationEmail);
+            var PasswordEditText = FindViewById<EditText>(Resource.Id.registrationPassword);
+            var signUpButton = FindViewById<Button>(Resource.Id.signUpButton);
+            var registrationView = FindViewById<RelativeLayout>(Resource.Id.registrationView);
+            var registrationHeader = FindViewById<TextView>(Resource.Id.registrationHeader);
+
+            Typeface robotoLightFont = Typeface.CreateFromAsset(Assets, "fonts/Roboto-Light.ttf");
+
+            NameEditText.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            LoginEditText.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            EmailEditText.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            PasswordEditText.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            signUpButton.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+            registrationHeader.SetTypeface(robotoLightFont, TypefaceStyle.Normal);
+
+            NameEditText.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            LoginEditText.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            EmailEditText.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            PasswordEditText.SetBackgroundResource(Resource.Drawable.EditTextStyle);
+            registrationView.SetBackgroundResource(Resource.Drawable.LoginPageStyle);
         }
     }
 }
