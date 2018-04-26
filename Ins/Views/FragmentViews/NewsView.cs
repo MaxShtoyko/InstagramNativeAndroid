bea874de1cf018cea6de9c662e125b76b5705f51
Max Shtoyko <maxym.shtoyko@jetsoftpro.com>
2018-04-26 20:03:59 +0300

    Implemented user icons in news view

--- Ins/Views/FragmentViews/NewsView.cs
+++ Ins/Views/FragmentViews/NewsView.cs
@@ -1,10 +1,15 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Support.V7.Widget;
 using Android.Views;
+using Android.Widget;
 using Ins.Core.Models;
+using Ins.Core.ViewModels;
 using Ins.Droid.Helpers.CameraHelpers;
+using Ins.Droid.Services;
 using MvvmCross.Droid.Views.Fragments;
+using Xamarin.Facebook.Login.Widget;
 
 namespace Ins.Droid.Views
 {
@@ -14,10 +19,16 @@ namespace Ins.Droid.Views
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container,
                                           Bundle savedInstanceState)
         {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            Typeface robotoLightFont = Typeface.CreateFromAsset(Context.Assets, "fonts/Roboto-Light.ttf");
+
             var photoAlbum = PhotoAlbum.GetPhotoAlbum();
             // Set our view from the "main" layout resource        
 
             var view = inflater.Inflate(Resource.Layout.NewsFragmentView, container, false);
+            var photoCard = inflater.Inflate(Resource.Layout.PhotoCard, container, false);
+
             var recyclerView = view.FindViewById(Resource.Id.recyclerView) as RecyclerView;
 
             var layoutManager = new LinearLayoutManager(Context);
@@ -26,10 +37,8 @@ namespace Ins.Droid.Views
             recyclerView.SetLayoutManager(layoutManager);          
             recyclerView.SetAdapter(adapter);
 
-            base.OnCreateView(inflater, container, savedInstanceState);
 
             return view;
         }
-
     }
 }
